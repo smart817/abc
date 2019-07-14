3a8592a62a44e5ac0979dfa79e73be0483fd82bf
smart817 <55507599@qq.com>
2019-07-14 12:13:31 +0800

    abc

---
+++ api/dati/controller/IndexController.php
@@ -0,0 +1,44 @@
+<?php
+// +----------------------------------------------------------------------
+// | ThinkCMF [ WE CAN DO IT MORE SIMPLE ]
+// +----------------------------------------------------------------------
+// | Copyright (c) 2013-2017 http://www.thinkcmf.com All rights reserved.
+// +----------------------------------------------------------------------
+// | Author: pl125 <xskjs888@163.com>
+// +----------------------------------------------------------------------
+
+namespace api\dati\controller;
+
+use EasyWeChat\Factory;
+use cmf\controller\RestBaseController;
+
+
+
+class IndexController extends RestBaseController
+{
+    public function aaa()
+    {
+        $code='sdfsdfsdf';
+        $config = [
+            'app_id' => 'wx5aa61c6d9c263087',
+            'secret' => 'd6be2230bdfb3d08a45077ef99114491',
+
+            // 下面为可选项
+            // 指定 API 调用返回结果的类型：array(default)/collection/object/raw/自定义类名
+            'response_type' => 'array',
+
+            'log' => [
+                'level' => 'debug',
+                'file' => __DIR__.'/wechat.log',
+            ],
+        ];
+        $miniProgram = Factory::miniProgram($config);
+        $data = $miniProgram->auth->session($code);
+        if (isset($data['errcode'])) {
+            //return $this->response->errorUnauthorized('code不正确或者code已经失效');
+        }else{
+            //array(['openid' => $data['openid'], 'session_key' => $data['session_key']]);
+        }
+        $this->success('请求成功!', $data);
+    }
+}
---
+++ api/dati/route.php
@@ -0,0 +1,20 @@
+<?php
+
+use think\facade\Route;
+
+//Route::resource('dati/articles', 'dati/Articles');
+Route::get('/',function(){
+    header("Access-Control-Allow-Origin:*");
+    //header('content-type:application/json');
+    $data = ['name'=>'thinkphp','url'=>'thinkphp.cn'];
+        // 指定json数据输出
+        return json(['code'=>1,'message'=>'操作完成','data'=>$data]);
+});
+Route::get('/',function(){
+    header("Access-Control-Allow-Origin:*");
+    //header('content-type:application/json');
+    $data = ['name'=>'thinkphp','url'=>'thinkphp.cn'];
+        // 指定json数据输出
+        return json(['code'=>1,'message'=>'操作完成','data'=>$data]);
+});
+Route::rule('aaa','dati/index/aaa');
