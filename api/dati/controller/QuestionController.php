1d0058a8a696027e31413a146c629cfa4d702271
smart817 <55507599@qq.com>
2019-07-15 15:29:24 +0800

    shoucang

--- api/dati/controller/QuestionController.php
+++ api/dati/controller/QuestionController.php
@@ -42,10 +42,10 @@ class QuestionController extends RestBaseController
         $finddata = Db::name('api_shoucang')->where('user_id',$data['user_id'])->where('question_id',$data['question_id'])->find();
         if(empty($finddata))
         {
-            $questions = Db::name('api_shoucang')->insert($data);
-            $this->success('错题记录', ['error_id'=> $questions ]);
+            $shoucangid = Db::name('api_shoucang')->insert($data);
+            $this->success('收藏成功');
         }else{
-            $this->success('你又一次答错了', ['error_id'=> $finddata['id'] ]);
+            $this->success('你已经收藏了');
         }
     }
 }
